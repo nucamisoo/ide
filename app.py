5734664210a5bfb1ef1310b32023bb1036a8ec45
nucamisoo <nucamisoo@gmail.com>
2022-03-28 00:48:24 +0900

    006 - fix by implementation with sheet_frame

--- app.py
+++ app.py
@@ -6,57 +6,73 @@ import csv
 import pprint
 
 
-def create_label_frame(container, row_lists):
-    label_frame = ttk.Frame(container)
-    for r_index, items in enumerate(row_lists):
-        for c_index, item in enumerate(items):
-            ttk.Label(label_frame, text=item).grid(row=r_index, column=c_index)
-    return label_frame
+def make_label_dict(label_headered_column_lists):
+    label_dict = {}
+    for column_list in label_headered_column_lists:
+        label_dict[column_list[0]] = [{'widget_type': 'Label', 'text': text} for text in column_list[1:]]
+    return label_dict
     
     
-def create_button_frame(container, filename_list):
-    button_frame = ttk.Frame(container)
-    
-    button_header_list = ['run', 'edit']
-    for c_index, c_name in enumerate(button_header_list):
-        ttk.Label(button_frame, text=c_name).grid(row=0, column=c_index)
-        for r_index, filename in enumerate(filename_list):
-            if c_name == 'run':
-                if '.csh' in filename:
-                    ttk.Button(button_frame, text=c_name).grid(row=r_index+1, column=c_index)
-                else:
-                    ttk.Label(button_frame, text='-').grid(row=r_index+1, column=c_index)
-            else:
-                ttk.Button(button_frame, text=c_name).grid(row=r_index+1, column=c_index)
-    
-    return button_frame
+def make_btn_run_cell_dlist(filename_list):
+    btn_run_cell_dlist = []
+    for filename in filename_list:
+        if '.csh' in filename:
+            btn_run_cell_dlist.append({'widget_type': 'Button', 'text': 'run'})
+        else:
+            btn_run_cell_dlist.append({'widget_type': 'Label', 'text': '-'})
+    return btn_run_cell_dlist
+
+
+def make_btn_edit_cell_dlist(filename_list):
+    btn_edit_cell_dlist = []
+    for _ in range(len(filename_list)):
+        btn_edit_cell_dlist.append({'widget_type': 'Button',
+                                    'text': 'edit'})
+    return btn_edit_cell_dlist
     
 
+def create_table(container, all_header_list, sheet_dict):
+    sheet_frame = ttk.Frame(container)
+    for c_index, c_name in enumerate(all_header_list):
+        ttk.Label(sheet_frame, text=c_name).grid(row=0, column=c_index)
+        for r_index, cell in enumerate(sheet_dict[c_name]):
+            cell_copied = cell.copy()
+            widgt_type = cell_copied.pop('widget_type')
+            if widgt_type == 'Label':
+                ttk.Label(sheet_frame, **cell_copied).grid(row=r_index+1, column=c_index)
+            elif widgt_type == 'Button':
+                ttk.Button(sheet_frame, **cell_copied).grid(row=r_index+1, column=c_index)
+    return sheet_frame
+    
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('IDE')
-    #root.geometry('600x400+50+50')
     
     with open('demo/csv/data.csv') as f:
         reader = csv.reader(f)
-        row_lists = [row for row in reader]
-        column_lists = [list(x) for x in zip(*row_lists)]  # transposed
         
-        label_frame = create_label_frame(root, row_lists)
+        # make data for label(_frame)
+        label_row_lists = [row for row in reader]
+        label_header_list = label_row_lists[0]  # ['index', 'task', 'path', 'filename']
+        label_headered_column_lists = [list(x) for x in zip(*label_row_lists)]  # transposed
         
-        label_header_list = row_lists[0]
-        filename_list = column_lists[label_header_list.index('filename')][1:]
-        pprint.pprint(filename_list)
-        button_frame = create_button_frame(root, filename_list)
-        
-        for widget in label_frame.winfo_children():
-            widget.config(anchor='w', padding=[10], width=15, borderwidth=1, relief='solid')
-        for widget in button_frame.winfo_children():
-            widget.config(padding=[10], width=15)
+        label_dict = make_label_dict(label_headered_column_lists)
             
-    label_frame.grid(row=0, column=0)
-    button_frame.grid(row=0, column=1)
+        # for button
+        filename_list = label_headered_column_lists[label_header_list.index('filename')][1:]
+        button_dict = {}
+        button_dict['run'] = make_btn_run_cell_dlist(filename_list)
+        button_dict['edit'] = make_btn_edit_cell_dlist(filename_list)
+        button_header_list = ['run', 'edit']
+        
+        # create table
+        sheet_header_list = label_header_list + button_header_list
+        sheet_dict = {**label_dict, **button_dict}
+        
+        sheet_frame = create_table(root, sheet_header_list, sheet_dict)
+    
+    sheet_frame.grid(row=0, column=0)
     
     root.mainloop()
 
