9c8d3d3fcb5b525ed6363e243e7c5fbe2065aa02
nucamisoo <nucamisoo@gmail.com>
2022-03-29 19:10:35 +0900

    007 - add context attribute to left-align labels in grid

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pprint
 def make_label_dict(label_headered_column_lists):
     label_dict = {}
     for column_list in label_headered_column_lists:
-        label_dict[column_list[0]] = [{'widget_type': 'Label', 'text': text} for text in column_list[1:]]
+        label_dict[column_list[0]] = [{'widget_type': 'Label', 'text': text, 'context': 'label_frame'} for text in column_list[1:]]
     return label_dict
     
     
@@ -17,17 +17,16 @@ def make_btn_run_cell_dlist(filename_list):
     btn_run_cell_dlist = []
     for filename in filename_list:
         if '.csh' in filename:
-            btn_run_cell_dlist.append({'widget_type': 'Button', 'text': 'run'})
+            btn_run_cell_dlist.append({'widget_type': 'Button', 'text': 'run', 'context': 'button_frame'})
         else:
-            btn_run_cell_dlist.append({'widget_type': 'Label', 'text': '-'})
+            btn_run_cell_dlist.append({'widget_type': 'Label', 'text': '-', 'context': 'button_frame'})
     return btn_run_cell_dlist
 
 
 def make_btn_edit_cell_dlist(filename_list):
     btn_edit_cell_dlist = []
     for _ in range(len(filename_list)):
-        btn_edit_cell_dlist.append({'widget_type': 'Button',
-                                    'text': 'edit'})
+        btn_edit_cell_dlist.append({'widget_type': 'Button', 'text': 'edit', 'context': 'button_frame'})
     return btn_edit_cell_dlist
     
 
@@ -38,8 +37,12 @@ def create_table(container, all_header_list, sheet_dict):
         for r_index, cell in enumerate(sheet_dict[c_name]):
             cell_copied = cell.copy()
             widgt_type = cell_copied.pop('widget_type')
+            context = cell_copied.pop('context')
             if widgt_type == 'Label':
-                ttk.Label(sheet_frame, **cell_copied).grid(row=r_index+1, column=c_index)
+                if context == 'label_frame':
+                    ttk.Label(sheet_frame, **cell_copied).grid(row=r_index+1, column=c_index, sticky=tk.W)
+                else:
+                    ttk.Label(sheet_frame, **cell_copied).grid(row=r_index+1, column=c_index)
             elif widgt_type == 'Button':
                 ttk.Button(sheet_frame, **cell_copied).grid(row=r_index+1, column=c_index)
     return sheet_frame
@@ -52,21 +55,21 @@ if __name__ == '__main__':
     with open('demo/csv/data.csv') as f:
         reader = csv.reader(f)
         
-        # make data for label(_frame)
+        # for labels
         label_row_lists = [row for row in reader]
         label_header_list = label_row_lists[0]  # ['index', 'task', 'path', 'filename']
         label_headered_column_lists = [list(x) for x in zip(*label_row_lists)]  # transposed
         
         label_dict = make_label_dict(label_headered_column_lists)
             
-        # for button
+        # for buttons
         filename_list = label_headered_column_lists[label_header_list.index('filename')][1:]
         button_dict = {}
         button_dict['run'] = make_btn_run_cell_dlist(filename_list)
         button_dict['edit'] = make_btn_edit_cell_dlist(filename_list)
         button_header_list = ['run', 'edit']
         
-        # create table
+        # create sheet table
         sheet_header_list = label_header_list + button_header_list
         sheet_dict = {**label_dict, **button_dict}
         
